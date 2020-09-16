8d4a1d02312b13801a95fda143892a28869da8c6
higker <deen.job@qq.com>
2020-09-16 21:20:59 +0800

    reverse string.

--- string/reverse.go
+++ string/reverse.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+// 反正字符串 把一个字符串的内容提供程序颠倒过来
+// https://www.geekxh.com/1.3.%E5%AD%97%E7%AC%A6%E4%B8%B2%E7%B3%BB%E5%88%97/301.html#_02%E3%80%81%E9%A2%98%E7%9B%AE%E5%9B%BE%E8%A7%A3
 func main() {
 	reverseString("您好啊")
 }
