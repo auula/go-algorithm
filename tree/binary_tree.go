c45cd6e953101a978ac86d5a67e9478fcf2c679b
Dings <deen.job@qq.com>
2020-10-09 19:08:56 +0800

    prev order traverse func

--- tree/binary_tree.go
+++ tree/binary_tree.go
@@ -6,6 +6,8 @@
 // 二叉树 查询树设计实现
 package main
 
+import "fmt"
+
 // 树的节点
 type treeNode struct {
 	Key         int
@@ -19,28 +21,45 @@ func newTreeNode(key int, value interface{}, node ...*treeNode) *treeNode {
 	return &treeNode{Key: key, Value: value}
 }
 
-func main() {
-	newTreeNode(1, "v1", nil, nil)
+func (n *treeNode) preOrder() {
+	fmt.Println(n) // 前序遍历
+	if n.Left != nil {
+		n.Left.preOrder()
+	}
+	if n.Right != nil {
+		n.Right.preOrder()
+	}
 }
 
+func main() {
+	bt := new(BinaryTree)
+	root := newTreeNode(1, "v1", nil)
+	n2 := newTreeNode(2, "v2", nil)
+	n3 := newTreeNode(3, "v3", nil)
+	n4 := newTreeNode(4, "v4", nil)
+	n5 := newTreeNode(5, "v5", nil)
+	root.Left = n3
+	root.Right = n4
+	n5.Left = n2
+	n4.Left = n5
+	bt.setRoot(root)
+	bt.preOrder() // 1,3,4,5,2
+}
+
+// BinaryTree 二叉树
 type BinaryTree struct {
 	Root   *treeNode
 	Length int
 }
 
-func (t *BinaryTree) Add(k int, v interface{}) {
-	if t.Root == nil {
-		return
-	}
-	if k > t.Root.Left.Key {
-
-	}
+func (bt *BinaryTree) setRoot(root *treeNode) {
+	bt.Root = root
+	//bt.Length++
 }
 
-func (t *BinaryTree) addLeftNode(node *treeNode) {
-
-}
-
-func (t *BinaryTree) addRightNode(node *treeNode) {
-
+// 前序遍历
+func (bt *BinaryTree) preOrder() {
+	if bt.Root != nil {
+		bt.Root.preOrder()
+	}
 }
