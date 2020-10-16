953cdcf4cff07cc0174010f9f7631543c55290e9
Dings <deen.job@qq.com>
2020-10-16 16:59:49 +0800

    add selection.go sort.

---
+++ array/insertion.go
@@ -0,0 +1,24 @@
+// Copyright (c) 2020 HigKer
+// Open Source: MIT License
+// Author: SDing <deen.job@qq.com>
+// Date: 2020/10/16 - 4:42 下午 - UTC/GMT+08:00
+
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{12, 13, 2, 49, 11, 4, 6, 3, 1}
+	insertionSort(arr)
+	fmt.Println(arr)
+}
+
+func insertionSort(arr []int) {
+	for i := 1; i < len(arr); i++ {
+		for j := i; j > 0; j-- {
+			if arr[j-1] > arr[j] {
+				arr[j-1], arr[j] = arr[j], arr[j-1]
+			}
+		}
+	}
+}
