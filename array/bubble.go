ea19753a56248672089187f6375f57979e0a5d27
Dings <deen.job@qq.com>
2020-10-19 17:45:00 +0800

    add notes

--- array/bubble.go
+++ array/bubble.go
@@ -30,6 +30,7 @@ func bubbleSort(arr []int) {
 func sort(arr []int) {
 	var flag bool
 	for i := 0; i < len(arr)-1; i++ {
+		// 冒泡是每次相邻的2个元素排
 		for j := 0; j < len(arr)-1-i; j++ {
 			if arr[j] > arr[j+1] {
 				arr[j], arr[j+1] = arr[j+1], arr[j]
@@ -43,4 +44,5 @@ func sort(arr []int) {
 			flag = false
 		}
 	}
+
 }
--- array/selection.go
+++ array/selection.go
@@ -10,7 +10,8 @@ import "fmt"
 
 func main() {
 	arr := []int{6, 5, 4, 3, 2, 1}
-	selectionSort(arr)
+	//selectionSort(arr)
+	sort(arr)
 	fmt.Println(arr)
 }
 
@@ -23,5 +24,19 @@ func selectionSort(arr []int) {
 			}
 		}
 		arr[i], arr[min] = arr[min], arr[i]
+	}
+}
+
+func sort(arr []int) {
+	// 这里的减一是因为需要通过下标方法元素 元素下标是从0开始的
+	for i := 0; i < len(arr)-1; i++ {
+		mid := i
+		// 而选择排序是 给定一个元素依次和后面的排序
+		// 如果发现逆向就交换这了元素的位置 直到整个走完
+		for j := i + 1; j < len(arr); j++ {
+			if arr[mid] > arr[j] {
+				arr[mid], arr[j] = arr[j], arr[mid]
+			}
+		}
 	}
 }
