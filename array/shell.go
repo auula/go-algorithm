bd154d9d75c331f4303941c64a4b928f34ea5f34
Dings <deen.job@qq.com>
2020-11-01 22:15:34 +0800

    add shell.go sort.

--- array/shell.go
+++ array/shell.go
@@ -46,3 +46,25 @@ func sortShell(arr []int) {
 		}
 	}
 }
+
+func ShellSort(a []int) {
+	n := len(a)
+	h := 1
+	for h < n/3 { //寻找合适的间隔h
+		h = 3*h + 1
+	}
+	for h >= 1 {
+		//将数组变为间隔h个元素有序
+		for i := h; i < n; i++ {
+			//间隔h插入排序
+			for j := i; j >= h && a[j] < a[j-h]; j -= h {
+				swap(a, j, j-h)
+			}
+		}
+		h /= 3
+	}
+}
+
+func swap(slice []int, i int, j int) {
+	slice[i], slice[j] = slice[j], slice[i]
+}
